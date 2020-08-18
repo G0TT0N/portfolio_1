5be284efce1f427caf45518768e32a1387fc2b9f
gotton <gotton92@mail.ru>
2020-08-18 19:13:49 +0300

    axios settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
-import { App } from "./components/App/App";
+import {render} from "react-dom";
+import {App} from "./components/App/App";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4200"
+    : "https://portfolio-1-server.herokuapp.com";
+// axios.defaults.baseURL = "https://portfolio-1-server.herokuapp.com";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4200"
+    : "https://g0tt0n.github.io";
+axios.defaults.headers.post["Access-Control-Allow-Methods"] =
+  "GET, POST, PATCH, PUT, DELETE, OPTIONS";
+axios.defaults.headers.post["Access-Control-Allow-Headers"] =
+  "Origin, X-Auth-Token,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With";
 
 render(<App />, document.getElementById("root"));
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { useHistory } from "react-router";
+import {useHistory} from "react-router";
+import axios from "axios";
 
 export const Home: React.FC = () => {
   const history = useHistory();
+
+  const test = () => {
+    axios
+      .post("/auth/register", {
+        email: "test2@mail.ru",
+        password: "123",
+      })
+      .then((res) => console.log("000000000000000000", res));
+  };
   return (
     <div>
       Home
       <button onClick={() => history.push("/login")}>login</button>
+      <button onClick={test}>axios</button>
     </div>
   );
 };
